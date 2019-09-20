8d6add32ec2e02f70774a4ecd52a7bfd7c2b52b1
msenecic <55552651+802-15@users.noreply.github.com>
2019-09-20 12:52:21 +0200

    First commit...

---
+++ main/wlan.h
@@ -0,0 +1,8 @@
+/* This file contains WLAN constants for ESP32
+ * It has to remain untracked by git for privacy issues
+ * Modify with your own values
+ */
+
+#define WIFI_SSID ssid
+#define WIFI_PASS pass
+
